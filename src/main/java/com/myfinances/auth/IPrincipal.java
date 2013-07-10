2c4861f138fa642001cd357ad5cc3b6d7d962b20
Sam Moore <samandmoore@gmail.com>
2013-07-09 22:14:01 -0400

    added ipricipal thing, not sure if i want to go that far just yet

---
+++ src/main/java/com/myfinances/auth/IPrincipal.java
@@ -0,0 +1,12 @@
+package com.myfinances.auth;
+
+/**
+ * TODO: documentation
+ *
+ * @author sam
+ * @since 7/9/13 10:10 PM
+ */
+public interface IPrincipal {
+    String getUserIdentifier();
+    String getFriendlyName();
+}
